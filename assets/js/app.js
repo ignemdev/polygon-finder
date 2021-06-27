3983e2546b441a45bc93330e67fdde6b70acf673
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2021-06-26 22:48:14 -0400

    fix: api key arreglada

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,29 @@
 //map init
-const polyMap = (screen.width >= 992) ? new polygonMap(document.querySelector('#map')) : null;
+const polyMap = screen.width >= 992 ? new polygonMap(document.querySelector('#map')) :
+    null;
 
 const drawBtn = document.querySelector('#draw-btn'),
     downloadBtn = document.querySelector('#download-btn'),
     polygonsInput = document.querySelector('#polygons-input'),
     markersInput = document.querySelector('#markers-input');
 
-const downloadMarkers = e => {
+const downloadMarkers = (e) => {
     const dataURL = `data:application/json,${JSON.stringify(polyMap.getMarkers())}`;
-    downloadBtn.setAttribute("download", "Markers.json");
-    downloadBtn.setAttribute("href", dataURL);
-}
+    downloadBtn.setAttribute('download', 'Markers.json');
+    downloadBtn.setAttribute('href', dataURL);
+};
 
 const readFile = (input, drawFunction) => {
     const fileReader = new FileReader();
-    fileReader.onload = e => polyMap[drawFunction](JSON.parse(e.target.result));
+    fileReader.onload = (e) => polyMap[drawFunction](JSON.parse(e.target.result));
     fileReader.readAsText(input.files[0]);
-}
+};
 
-const drawPolysAndMarkers = e => {
+const drawPolysAndMarkers = (e) => {
     polyMap.cleanMap();
     readFile(markersInput, 'setMarkers');
     readFile(polygonsInput, 'setPolygons');
-}
+};
 
 downloadBtn.addEventListener('click', downloadMarkers);
 
