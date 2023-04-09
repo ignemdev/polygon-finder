4bd632f4e144a64708ebb99ddb4837f9d69af314
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2023-04-09 10:39:31 -0400

    migrating to leaflet
    -initMap
    -setCurrentPosition
    -setCenter
    -setZoom

--- assets/js/PolygonMap.js
+++ assets/js/PolygonMap.js
@@ -1,237 +1,92 @@
 class polygonMap {
-    #_style = [
-        {
-            "elementType": "geometry",
-            "stylers": [
-                {
-                    "color": "#f5f5f5"
-                }
-            ]
-        },
-        {
-            "elementType": "labels.icon",
-            "stylers": [
-                {
-                    "visibility": "off"
-                }
-            ]
-        },
-        {
-            "elementType": "labels.text.fill",
-            "stylers": [
-                {
-                    "color": "#616161"
-                }
-            ]
-        },
-        {
-            "elementType": "labels.text.stroke",
-            "stylers": [
-                {
-                    "color": "#f5f5f5"
-                }
-            ]
-        },
-        {
-            "featureType": "administrative.land_parcel",
-            "elementType": "labels.text.fill",
-            "stylers": [
-                {
-                    "color": "#bdbdbd"
-                }
-            ]
-        },
-        {
-            "featureType": "poi",
-            "elementType": "geometry",
-            "stylers": [
-                {
-                    "color": "#eeeeee"
-                }
-            ]
-        },
-        {
-            "featureType": "poi",
-            "elementType": "labels.text.fill",
-            "stylers": [
-                {
-                    "color": "#757575"
-                }
-            ]
-        },
-        {
-            "featureType": "poi.park",
-            "elementType": "geometry",
-            "stylers": [
-                {
-                    "color": "#e5e5e5"
-                }
-            ]
-        },
-        {
-            "featureType": "poi.park",
-            "elementType": "labels.text.fill",
-            "stylers": [
-                {
-                    "color": "#9e9e9e"
-                }
-            ]
-        },
-        {
-            "featureType": "road",
-            "elementType": "geometry",
-            "stylers": [
-                {
-                    "color": "#ffffff"
-                }
-            ]
-        },
-        {
-            "featureType": "road.arterial",
-            "elementType": "labels.text.fill",
-            "stylers": [
-                {
-                    "color": "#757575"
-                }
-            ]
-        },
-        {
-            "featureType": "road.highway",
-            "elementType": "geometry",
-            "stylers": [
-                {
-                    "color": "#dadada"
-                }
-            ]
-        },
-        {
-            "featureType": "road.highway",
-            "elementType": "labels.text.fill",
-            "stylers": [
-                {
-                    "color": "#616161"
-                }
-            ]
-        },
-        {
-            "featureType": "road.local",
-            "elementType": "labels.text.fill",
-            "stylers": [
-                {
-                    "color": "#9e9e9e"
-                }
-            ]
-        },
-        {
-            "featureType": "transit.line",
-            "elementType": "geometry",
-            "stylers": [
-                {
-                    "color": "#e5e5e5"
-                }
-            ]
-        },
-        {
-            "featureType": "transit.station",
-            "elementType": "geometry",
-            "stylers": [
-                {
-                    "color": "#eeeeee"
-                }
-            ]
-        },
-        {
-            "featureType": "water",
-            "elementType": "geometry",
-            "stylers": [
-                {
-                    "color": "#c9c9c9"
-                }
-            ]
-        },
-        {
-            "featureType": "water",
-            "elementType": "labels.text.fill",
-            "stylers": [
-                {
-                    "color": "#9e9e9e"
-                }
-            ]
-        }
-    ]
-    #_map;
-    #_markers = [];
-    #_polygons = [];
+  #_map;
+  #_markers = [];
+  #_polygons = [];
 
-    constructor(mapContainer) {
-        this.initMap(mapContainer);
+  constructor(mapContainer) {
+    this.initMap(mapContainer);
+  }
+
+  initMap(mapContainer) {
+    this.#_map = L.map(mapContainer, {
+      center: [18.5112853, -69.8911881],
+      zoom: 6,
+    });
+
+    L.tileLayer(
+      "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png",
+      {
+        maxZoom: 19,
+      }
+    ).addTo(this.#_map);
+
+    // this.#_map = new google.maps.Map(mapContainer, {
+    //   zoom: 6,
+    //   styles: this.#_style,
+    //   disableDefaultUI: true,
+    //   center: { lat: 18.5112853, lng: -69.8911881 },
+    // });
+
+    this.setCurrentPosition();
+  }
+
+  cleanMap() {
+    this.#_map.data.forEach((feature) => this.#_map.data.remove(feature));
+    this.#_markers.forEach((marker) => marker.setMap(null));
+    this.#_markers, (this.#_polygons = []);
+  }
+
+  setCurrentPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const {
+          coords: { latitude, longitude },
+        } = position;
+        this.setCenter({ lat: latitude, lng: longitude });
+        this.setZoom(15);
+      });
     }
+  }
 
-    initMap(mapContainer) {
-        this.#_map = new google.maps.Map(mapContainer, {
-            zoom: 6,
-            styles: this.#_style,
-            disableDefaultUI: true,
-            center: { lat: 18.5112853, lng: -69.8911881 }
-        });
+  setCenter = ({ lat, lng }) => this.#_map.setView([lat, lng], 0);
 
-        this.setCurrentPosition();
-    };
+  setZoom = (amount) => this.#_map.setZoom(amount);
 
-    cleanMap() {
-        this.#_map.data.forEach(feature => this.#_map.data.remove(feature));
-        this.#_markers.forEach(marker => marker.setMap(null));
-        this.#_markers, this.#_polygons = [];
-    };
+  setPolygons(parsedGeojson) {
+    this.#_map.data.addGeoJson(parsedGeojson);
+    this.#_map.data.forEach((feature) => {
+      const polygon = new google.maps.Polygon({
+        path: feature.getGeometry().getAt(0).getArray(),
+        label: feature.j.name,
+        description: feature.j.description || feature.j.desc,
+      });
+      this.#_polygons.push(polygon);
+    });
+  }
 
-    setCurrentPosition() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(position => {
-                const { coords: { latitude, longitude } } = position;
-                this.setCenter({ lat: latitude, lng: longitude });
-                this.setZoom(15);
-            });
-        }
-    }
+  setMarkers(parsedJson) {
+    this.setCenter(parsedJson[0].position);
+    parsedJson.forEach((element) => {
+      const marker = new google.maps.Marker({
+        position: element.position,
+        map: this.#_map,
+        title: element.name,
+      });
+      this.#_markers.push(marker);
+    });
+  }
 
-    setCenter = (position) => this.#_map.setCenter(position)
-
-    setZoom = (amount) => this.#_map.setZoom(amount);
-
-    setPolygons(parsedGeojson) {
-        this.#_map.data.addGeoJson(parsedGeojson);
-        this.#_map.data.forEach(feature => {
-            const polygon = new google.maps.Polygon({
-                path: feature.getGeometry().getAt(0).getArray(),
-                label: feature.j.name,
-                description: feature.j.description || feature.j.desc
-            });
-            this.#_polygons.push(polygon);
-        });
-    };
-
-    setMarkers(parsedJson) {
-        this.setCenter(parsedJson[0].position);
-        parsedJson.forEach(element => {
-            const marker = new google.maps.Marker({
-                position: element.position,
-                map: this.#_map,
-                title: element.name
-            });
-            this.#_markers.push(marker);
-        });
-    };
-
-    getMarkers() {
-        return this.#_markers.map(marker => {
-            const isInside = polygon => google.maps.geometry.poly.containsLocation(marker.position, polygon);
-            const polygonFound = this.#_polygons.find(isInside);
-            return {
-                name: marker.title,
-                belongsTo: (polygonFound) ? polygonFound.label : 'NF',
-                lat: marker.position.lat(),
-                lng: marker.position.lng()
-            }
-        });
-    };
+  getMarkers() {
+    return this.#_markers.map((marker) => {
+      const isInside = (polygon) =>
+        google.maps.geometry.poly.containsLocation(marker.position, polygon);
+      const polygonFound = this.#_polygons.find(isInside);
+      return {
+        name: marker.title,
+        belongsTo: polygonFound ? polygonFound.label : "NF",
+        lat: marker.position.lat(),
+        lng: marker.position.lng(),
+      };
+    });
+  }
 }
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,32 @@
 //map init
-const polyMap = screen.width >= 992 ? new polygonMap(document.querySelector('#map')) :
-    null;
+const polyMap =
+  screen.width >= 992 ? new polygonMap(document.querySelector("#map")) : null;
 
-const drawBtn = document.querySelector('#draw-btn'),
-    downloadBtn = document.querySelector('#download-btn'),
-    polygonsInput = document.querySelector('#polygons-input'),
-    markersInput = document.querySelector('#markers-input');
+const drawBtn = document.querySelector("#draw-btn"),
+  downloadBtn = document.querySelector("#download-btn"),
+  polygonsInput = document.querySelector("#polygons-input"),
+  markersInput = document.querySelector("#markers-input");
 
 const downloadMarkers = (e) => {
-    const dataURL = `data:application/json,${JSON.stringify(polyMap.getMarkers())}`;
-    downloadBtn.setAttribute('download', 'Markers.json');
-    downloadBtn.setAttribute('href', dataURL);
+  const dataURL = `data:application/json,${JSON.stringify(
+    polyMap.getMarkers()
+  )}`;
+  downloadBtn.setAttribute("download", "Markers.json");
+  downloadBtn.setAttribute("href", dataURL);
 };
 
 const readFile = (input, drawFunction) => {
-    const fileReader = new FileReader();
-    fileReader.onload = (e) => polyMap[drawFunction](JSON.parse(e.target.result));
-    fileReader.readAsText(input.files[0]);
+  const fileReader = new FileReader();
+  fileReader.onload = (e) => polyMap[drawFunction](JSON.parse(e.target.result));
+  fileReader.readAsText(input.files[0]);
 };
 
 const drawPolysAndMarkers = (e) => {
-    polyMap.cleanMap();
-    readFile(markersInput, 'setMarkers');
-    readFile(polygonsInput, 'setPolygons');
+  polyMap.cleanMap();
+  readFile(markersInput, "setMarkers");
+  readFile(polygonsInput, "setPolygons");
 };
 
-downloadBtn.addEventListener('click', downloadMarkers);
+downloadBtn.addEventListener("click", downloadMarkers);
 
-drawBtn.addEventListener('click', drawPolysAndMarkers);
+drawBtn.addEventListener("click", drawPolysAndMarkers);
